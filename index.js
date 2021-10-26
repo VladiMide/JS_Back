d33d5885c37f3399d2aaf2f616d746562c7c51b9
VladiMide <76111733+VladiMide@users.noreply.github.com>
2021-10-26 18:09:36 +0300

    practice 2

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const { consumers } = require('stream');
+const { rawListeners } = require('process');
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("DataBase", "postgres", "1245", {
+  dialect: "postgres",
+  host: "localhost"
+});
+const db1 = sequelize.define("Db1", {
+  title: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
+});
+sequelize.sync().then(result=>console.log(result))
+.catch(err=> console.log(err));
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./localstorage');
+  }
+
+if (!!localStorage.getItem("Arr")){
+    var Arr = localStorage.getItem("Arr");
+}
+else{
+    var Arr = [];
+}
 
 const app = express();
 
@@ -74,6 +107,118 @@ app.post('/reverseArray', (req,res) => {
     res.status(200).json({message: 'Reverse Array:' + reverseArray});
 })
 
+app.post('/strings', (req,res) =>{
+    let string = req.body.str;
+    Arr.push(string);
+    localStorage.setItem("Arr", Arr);
+    res.status(200).json({message: "Done!"});
+})
+
+app.get('/strings', (req,res) => {
+    res.status(200).json({message: 'String Array is ' + Arr.toString()});
+})
+
+app.delete('/strings/:index', (req,res) => {
+    index = req.params.index - 1;
+    if (index != Arr.length - 1){
+    Arr = Arr.slice(0,index).concat(Arr.slice(index + 1));
+    }
+    else{
+        Arr.pop();
+    }
+})
+
+app.delete('/strings', (req,res) => {
+    Arr = [];
+    res.status(200).json({message:'All done!'});
+})
+
+app.post('/db', (req, res) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    let Db1 = db1.build({
+      title: title,
+      description: description
+    });
+    Db1.save();
+    res.status(200).json({message: "Created DataBase", title: title, description: description})
+    })
+    
+    app.get('/db',async (req, res) => {
+      let title = req.body.title;
+      let Db1 = await db1.findAll({
+        raw: true,
+        where : {
+          title: title
+        }
+      })
+      Db1 = Db1[0];
+      if (Db1 != undefined){
+        res.status(200).json({message: "Your DataBase", title: Db1.title, description: Db1.description})
+      }
+      else {
+        res.status(404).json({message: "DataBase not found"})
+      }
+      
+    })
+    
+    app.put('/db',async (req, res) => {
+      let title = req.body.title;
+      let description = req.body.description;
+      let Db1 = await db1.findAll({
+        raw: true,
+        where : {
+          title: title
+        }
+      })
+      if (Db1[0] != undefined){
+         await db1.update(
+          {description : description}, 
+          { where : {
+            title: title
+          }
+        })
+        res.status(200).json({message: "Changed DataBase", title: title, description: todo.description })
+      }
+      else {
+        res.status(404).json({message: "DataBase not found"})
+      }
+    })
+    
+    app.delete('/db',async (req, res) => {
+      let title = req.body.title;
+      let Db1 =await db1.findAll({
+        where : {
+          title: title
+        }
+      })
+      Db1 = Db1[0];
+      if (Db1 != undefined){
+        db1.destroy({
+          where : {
+            title: title
+          }
+        });
+        res.status(200).json({message: "Deleted DataBase", title: title})
+      }
+      else {
+        res.status(404).json({message: "DataBase not found"})
+      }
+    })
+    
+    app.delete('/db/all',async (req, res) => {
+      let title = req.body.title;
+      let Db1 =await db1.findAll({
+      })
+      if (Db1[0] != undefined){
+        db1.destroy({
+          truncate: true,
+      })
+      res.status(200).json({message: "Deleted all DataBases"})
+      }
+      else
+      res.status(200).json({message: "Table is empty"})
+    })
 
 http.createServer(app).listen(3000, () => {
   console.log('Server is working on port 3000');
